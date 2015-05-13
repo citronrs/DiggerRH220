53858b851a14d24f3963b97abd652dc7cbc14b99
Crodyt <crodyt@rose-hulman.edu>
2015-05-13 19:37:54 -0400

    Added all new classes and also fixed some errors and bugs in Digger

--- src/ControlBar.java
+++ src/ControlBar.java
@@ -5,14 +5,13 @@ import javax.swing.JPanel;
 public class ControlBar extends JPanel{
 	
 	private JButton newButton;
-	
-	
 	JPanel controlBar;
 	
 	public ControlBar(){
 		controlBar = new JPanel();
+		JButton Button = new JButton();
+		this.add(Button);
 		setSize(100, 500);
-		//butthole
 	}
 
 }
--- src/Digger.java
+++ src/Digger.java
@@ -1,32 +1,43 @@
-import java.awt.Container;
-import java.awt.GridLayout;
-
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-  
-public class Digger extends JFrame{
-	
-	public static void main(String args[]){
-		new Digger();
+public class Digger extends JFrame {
+
+	Hero hero = new Hero();
+
+	public static void main(String args[]) {
+		Digger d = new Digger();
+		while (true) {
+			d.gethero().moving();
+			d.repaint();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException exception) {
+				// TODO Auto-generated catch-block stub.
+				exception.printStackTrace();
+			}
+		}
 	}
-	
-	public Digger(){
+
+	public Digger() {
 		super("Digger (By T.Crody and R.Citron)");
-		
+
 		ControlBar ControlBar;
 		
-		Container mainframe = getContentPane();
-		mainframe.setLayout(new GridLayout(2, 1));
+		JPanel game = new JPanel();
+		
 		ControlBar = new ControlBar();
-		mainframe.add(ControlBar);	
-
-		
-		setResizable(true);
-		setSize(500,500);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
+		add(ControlBar);
+		this.hero = new Hero();
+		game.add(hero);
+		add(game);
+		setSize(300, 400);
 		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public Hero gethero(){
+		return hero;
 	}
 
 }
